897da8291889c51e5e584c1092ca1e85ca1ade77
cgsabri <g-49295306@moe-dl.edu.my>
2026-07-23 09:40:17 +0800

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "delima-v1";
+const CACHE_NAME = "delima-v2";
 
 const FILES = [
   "./",
@@ -8,31 +8,32 @@ const FILES = [
   "./manifest.json"
 ];
 
-
 self.addEventListener("install", event => {
+  self.skipWaiting();
 
   event.waitUntil(
-
-    caches
-      .open(CACHE_NAME)
-      .then(cache =>
-        cache.addAll(FILES)
-      )
-
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(FILES))
   );
-
 });
 
-
-self.addEventListener("fetch", event => {
-
-  event.respondWith(
-
-    fetch(event.request)
-      .catch(() =>
-        caches.match(event.request)
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
       )
-
+    )
   );
 
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+  );
 });
